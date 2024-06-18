c95807756ef1b34447b996cd97135a662389416a
Nasim <nasim.programmer24.com>
2024-06-19 01:38:57 +0600

    made Update Role Page

---
+++ src/components/Dashboard/Table/Row/UserDataRow.jsx
@@ -0,0 +1,99 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import UpdateUserModal from "../../Modal/UpdateUserModal";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import useAuth from "../../../../hooks/useAuth";
+const UserDataRow = ({ user, refetch }) => {
+  const { user: loggedInUser } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+  const axiosSecure = useAxiosSecure();
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async (role) => {
+      const { data } = await axiosSecure.patch(
+        `/users/update/${user?.email}`,
+        role
+      );
+      return data;
+    },
+    onSuccess: () => {
+      refetch();
+      toast.success("User role updated successfully!!");
+    },
+  });
+
+  // modal handler
+  const modalHandler = async (selected) => {
+    if (loggedInUser?.email === user?.email) {
+      toast.error("Action Not Allow!");
+      return setIsOpen(false);
+    }
+    const userRole = {
+      role: selected,
+      status: "Verified",
+    };
+
+    try {
+      await mutateAsync(userRole);
+      setIsOpen(false);
+    } catch (err) {
+      toast.error(err.message);
+      setIsOpen(false);
+    }
+  };
+  return (
+    <tr>
+      <td className='px-5 py-3 border-b border-gray-200 bg-white text-sm text-left'>
+        <p className='text-gray-900 whitespace-no-wrap'>{user?.name}</p>
+      </td>
+      <td className='px-5 py-3 border-b border-gray-200 bg-white text-sm text-left'>
+        <p className='text-gray-900 whitespace-no-wrap'>{user?.email}</p>
+      </td>
+      <td className='flex justify-center items-center px-5 py-3  border-b border-gray-200 bg-white '>
+        <img src={user?.photo} className='w-20 h-20 rounded-full' alt='' />
+      </td>
+      <td className='px-5 py-3 border-b border-gray-200 bg-white text-sm text-left'>
+        <p className='text-gray-900 whitespace-no-wrap'>{user?.role}</p>
+      </td>
+      <td className='px-5 py-3 border-b border-gray-200 bg-white text-sm text-left'>
+        {user?.status ? (
+          <p
+            className={`${
+              user.status === "Verified" ? "text-green-500" : "text-yellow-500"
+            } whitespace-no-wrap`}>
+            {user.status}
+          </p>
+        ) : (
+          <p className='text-red-500 whitespace-no-wrap'>Unavailable</p>
+        )}
+      </td>
+
+      <td className='px-5 py-3 border-b border-gray-200 bg-white text-sm text-left'>
+        <button
+          onClick={() => setIsOpen(true)}
+          className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
+          <span
+            aria-hidden='true'
+            className='absolute inset-0 bg-green-200 opacity-50 rounded-full'></span>
+          <span className='relative'>Update Role</span>
+        </button>
+        {/* Update User Modal */}
+        <UpdateUserModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          modalHandler={modalHandler}
+          user={user}
+        />
+      </td>
+    </tr>
+  );
+};
+
+UserDataRow.propTypes = {
+  user: PropTypes.object,
+  refetch: PropTypes.func,
+};
+
+export default UserDataRow;
--- src/pages/Dashboard/Admin/Users/Users.jsx
+++ src/pages/Dashboard/Admin/Users/Users.jsx
@@ -1,7 +1,137 @@
+import { Helmet } from "react-helmet-async";
+import UserDataRow from "../../../../components/Dashboard/Table/Row/UserDataRow";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const Users = () => {
+  const axiosSecure = useAxiosSecure();
+  const {
+    data: users = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/users`);
+      return data;
+    },
+  });
+
+  const renderSkeletons = (count) => {
+    return Array.from({ length: count }).map((_, index) => (
+      <tr key={index}>
+        <td className='p-3'>
+          <Skeleton height={25} />
+        </td>
+        <td className='p-3'>
+          <Skeleton height={25} />
+        </td>
+        <td className='p-3 text-center'>
+          <Skeleton circle={true} height={50} width={50} />
+        </td>
+        <td className='p-3'>
+          <Skeleton height={25} />
+        </td>
+        <td className='p-3'>
+          <Skeleton height={25} />
+        </td>
+        <td className='p-3'>
+          <Skeleton height={25} />
+        </td>
+      </tr>
+    ));
+  };
+
+  const renderHeaderSkeletons = () => (
+    <tr>
+      <th className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+        <Skeleton height={25} width={100} />
+      </th>
+      <th className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+        <Skeleton height={25} width={100} />
+      </th>
+      <th className='p-3 text-center bg-white border-y border-gray-200 text-sm uppercase font-normal'>
+        <Skeleton height={25} width={100} />
+      </th>
+      <th className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+        <Skeleton height={25} width={100} />
+      </th>
+      <th className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+        <Skeleton height={25} width={100} />
+      </th>
+      <th className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+        <Skeleton height={25} width={100} />
+      </th>
+    </tr>
+  );
+
   return (
     <div>
-      <h3 className='text-3xl'>All Users Page is Here</h3>
+      <Helmet>
+        <title>PetLoversHub || Users</title>
+      </Helmet>
+      <h3 className='text-3xl md:text-5xl text-center font-bold dark:text-white'>
+        List All Users of PetLoversHub
+      </h3>
+      <div className='py-8'>
+        <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 w-full text-center overflow-x-auto'>
+          <div className='inline-block mx-auto shadow rounded-lg overflow-hidden'>
+            <table className='w-full leading-normal'>
+              <thead>
+                {isLoading ? (
+                  renderHeaderSkeletons()
+                ) : (
+                  <tr>
+                    <th
+                      scope='col'
+                      className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+                      Name
+                    </th>
+                    <th
+                      scope='col'
+                      className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+                      Email
+                    </th>
+                    <th
+                      scope='col'
+                      className='p-3 text-center bg-white border-y border-gray-200 text-sm uppercase font-normal'>
+                      Profile Picture
+                    </th>
+                    <th
+                      scope='col'
+                      className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+                      Role
+                    </th>
+                    <th
+                      scope='col'
+                      className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+                      Status
+                    </th>
+                    <th
+                      scope='col'
+                      className='p-3 bg-white border-y border-gray-200 text-gray-800 text-left text-sm uppercase font-normal'>
+                      Action
+                    </th>
+                  </tr>
+                )}
+              </thead>
+              <tbody>
+                {isLoading
+                  ? renderSkeletons(5)
+                  : users.map((user) => (
+                      <UserDataRow
+                        key={user?._id}
+                        user={user}
+                        refetch={refetch}
+                      />
+                    ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
