a712714f745cf92dc8aafa1de901c22192d2ab59
md-nasim-mondal <nasim.programmer24@gmail.com>
2024-11-18 23:17:43 +0600

    solve another ui design issue for small device navbar

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -168,7 +168,7 @@ const Navbar = () => {
                 menuVisible ? "" : "hidden"
               }`}>
               <ul className='flex flex-col w-52 ml-auto md:w-full md:ml-0 font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-b-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-                <li className='md:hidden'>
+                <li className='md:hidden mb-2'>
                   <ThemeSwitcher />
                 </li>
                 {navItems}
